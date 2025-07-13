d1438b119192aa63dfab328bcd548f76ded2156d
AniHovsepyann <anihovsepyan004@mail.ru>
2025-07-13 21:36:42 +0400

    Added runnind line

--- src/components/Home/KidsOff/KidsOff.jsx
+++ src/components/Home/KidsOff/KidsOff.jsx
@@ -45,29 +45,49 @@ export default function KidsOff() {
 
   return (
     // <FloatingBackgroundSection sectionRef={sectionRef} parentStyles={styles} image_1={"https://static.wixstatic.com/media/c837a6_936329eced864c0abedbf89844390c72~mv2.jpg/v1/fill/w_347,h_260,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/c837a6_936329eced864c0abedbf89844390c72~mv2.jpg"}/>
-    <section ref={sectionRef} className={`${styles["floating-background-section"]} ${styles["floating-background-section"]}`}>
-      <div ref={left_topRef} className={styles["left-top"]}>
-        <h2>Step-up Your Stride</h2>
-        <p>Premium Athletic Footwear</p>
-        <NavLink to={SHOPNOW}>SHOP NOW</NavLink>
-      </div>
-      <div className={styles['right-bottom']}>
-        <NavLink to={PRODUCT} className={styles["img-container"]}>
-          <button className={styles.sale}>SALE</button>
-          <img className={styles["image-1"]} src={"https://static.wixstatic.com/media/c837a6_936329eced864c0abedbf89844390c72~mv2.jpg/v1/fill/w_347,h_260,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/c837a6_936329eced864c0abedbf89844390c72~mv2.jpg"} alt="shoe" />
-          <img className={styles["image-2"]} src="https://static.wixstatic.com/media/c837a6_60f18667fe64458eb7ab6a131d3354d4~mv2.jpg/v1/fill/w_347,h_260,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/c837a6_60f18667fe64458eb7ab6a131d3354d4~mv2.jpg" alt="shoe" />
-        </NavLink>
-        <div className={styles["product-data"]}>
-          <NavLink to={PRODUCT} className={styles["name-and-price"]}>
-            <h4>MARATHON MASTER</h4>
-            <span className={styles["price-pt"]}>
-              <ins>$140.00</ins>
-              <del>$160.00</del>
-            </span>
+    <section>
+      <div ref={sectionRef} className={`${styles["floating-background-section"]} ${styles["floating-background-section"]}`}>
+        <div ref={left_topRef} className={styles["left-top"]}>
+          <h2>Step-up Your Stride</h2>
+          <p>Premium Athletic Footwear</p>
+          <NavLink to={SHOPNOW}>SHOP NOW</NavLink>
+        </div>
+        <div className={styles['right-bottom']}>
+          <NavLink to={PRODUCT} className={styles["img-container"]}>
+            <button className={styles.sale}>SALE</button>
+            <img className={styles["image-1"]} src={"https://static.wixstatic.com/media/c837a6_936329eced864c0abedbf89844390c72~mv2.jpg/v1/fill/w_347,h_260,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/c837a6_936329eced864c0abedbf89844390c72~mv2.jpg"} alt="shoe" />
+            <img className={styles["image-2"]} src="https://static.wixstatic.com/media/c837a6_60f18667fe64458eb7ab6a131d3354d4~mv2.jpg/v1/fill/w_347,h_260,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/c837a6_60f18667fe64458eb7ab6a131d3354d4~mv2.jpg" alt="shoe" />
           </NavLink>
-          <button className={styles.add}>
-            ADD TO CART
-          </button>
+          <div className={styles["product-data"]}>
+            <NavLink to={PRODUCT} className={styles["name-and-price"]}>
+              <h4>MARATHON MASTER</h4>
+              <span className={styles["price-pt"]}>
+                <ins>$140.00</ins>
+                <del>$160.00</del>
+              </span>
+            </NavLink>
+            <button className={styles.add}>
+              ADD TO CART
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={styles["marquee-container"]}>
+        <div className={styles["marquee-track"]}>
+          <ul className={styles["running-line"]}>
+            {
+              Array(7).fill().map((_, i) => (
+                <li key={`1-${i}`}>NEW ENERGY, SAME HIGH QUALITY</li>
+              ))
+            }
+          </ul>
+          <ul className={styles["running-line"]}>
+            {
+              Array(7).fill().map((_, i) => (
+                <li key={`2-${i}`}>NEW ENERGY, SAME HIGH QUALITY</li>
+              ))
+            }
+          </ul>
         </div>
       </div>
     </section>
