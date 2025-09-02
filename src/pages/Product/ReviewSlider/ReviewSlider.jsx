055415f2adbe4e8bebdf78c8ff481cafefb2f048
AniHovsepyann <anihovsepyan004@mail.ru>
2025-09-02 22:43:44 +0400

    Fix comments stars

--- src/pages/Product/ReviewSlider/ReviewSlider.jsx
+++ src/pages/Product/ReviewSlider/ReviewSlider.jsx
@@ -20,7 +20,17 @@ export default function Reviews({ reviews }) {
                 .
                 <span className="date">{elm.date}</span>
               </div>
-              <div className="stars">{elm.rate} stars</div>
+              {/* <div className="stars">{Array(+elm.rate).fill().map((_) => (<i className="bi bi-star-fill"></i>))} {Array(5 - elm.rate).fill().map((_) => (<i class="bi bi-star"></i>))}</div> */}
+              <div className="stars">
+                {[...Array(5)].map((_, i) =>
+                  i < elm.rate ? (
+                    <i key={i} className="bi bi-star-fill fs-3"></i>
+                  ) : (
+                    <i key={i} className="bi bi-star fs-3"></i>
+                  )
+                )}
+              </div>
+
               <div className="title">{elm.title}</div>
               <div className="description">{elm.review}</div>
               <div className="interactions">Was this helpful?</div>
